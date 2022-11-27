aca922d842068b805f4dc55e579d7c62810d3256
Oleg Tsvirkun <dr.olegtsvirkun@gmail.com>
2022-11-27 22:28:10 +0200

    react init app

---
+++ react app/currency_exchange/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './HomePage.module.scss';
+
+export const HomePage = ({}) => (
+  <div className={styles.homePage}>
+    HomePage Component
+  </div>
+);
+
---
+++ react app/currency_exchange/templates/TemplateName.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './TemplateName.module.scss';
+
+export const TemplateName = ({}) => (
+  <div className={styles.templateName}>
+    TemplateName Component
+  </div>
+);
+
