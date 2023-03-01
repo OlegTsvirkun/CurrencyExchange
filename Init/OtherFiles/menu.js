df0065941807401860fa0c2e8869ddf362d2f1b7
OlegTsvirkun <dr.olegtsvirkun@gmail.com>
2023-03-01 12:56:42 +0200

    remove app folder

---
+++ Init/OtherFiles/menu.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector('#menu');
+const menuList = document.querySelector('.menu__list');
+const menuItem = document.querySelector('.menu__item');
+const helper = document.querySelector('.menu__helper');
+menu.addEventListener('click', () => {
+    menuList.classList.toggle('menu-open');
+    // menuList.classList.toggle('notactive');
+    menuItem.classList.toggle('menu-click');
+    menuItem.classList.toggle('hide');
+    helper.style.width = menu.offsetWidth + 'px';
+
+
+  
+
+})
+window.addEventListener('resize', () => helper.style.width = menu.offsetWidth + 'px')
+document.addEventListener('click', (e) => {
+    if (e.path.includes(menu)) {
+        menuList.classList.remove('menu-open');
+        menuItem.classList.remove('menu-click');
+        menuItem.classList.add('hide');
+    };
+
+})
